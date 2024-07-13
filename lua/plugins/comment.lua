468b7e42deab06c45c35ee16b391913acf538da6
Razkaroth <rocker.ikaros@gmail.com>
2024-07-13 02:57:49 -0600

    updates Comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,9 +1,17 @@
-return  {
-    "numToStr/Comment.nvim",
-    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
-    config = function()
-      require("Comment").setup {
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      }
-    end,
-  }
+return {
+	"numToStr/Comment.nvim",
+	lazy = false,
+	dependencies = {
+		{
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			opts = {
+				enable_autocmd = false,
+			},
+		},
+	},
+	config = function()
+		require("Comment").setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
+}
